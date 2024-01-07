de8c4b1cdfaf64bf77983ae804550dbb1bad690f
ccccbe <1051568734@qq.com>
2024-01-08 00:02:42 +0800

    fuck u

--- hazard.v
+++ hazard.v
@@ -18,7 +18,6 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-`include "defines.vh"
 
 
 module hazard(
@@ -30,65 +29,30 @@ module hazard(
 	output wire forwardaD,forwardbD,
 	output wire stallD,
 	//execute stage
-	output wire forwardajalE,
-
-	input wire [1:0] hilo_weE,
-	output wire[1:0] forwardhiloE,
-
-	output wire stallE,
-
 	input wire[4:0] rsE,rtE,
 	input wire[4:0] writeregE,
 	input wire regwriteE,
 	input wire memtoregE,
 	output reg[1:0] forwardaE,forwardbE,
 	output wire flushE,
-
-
-	output wire div_start,
-	input wire div_ready,//start and ready should exchange
-	input wire[7:0] alucontrolE,
 	//mem stage
-	output wire stallM,
-	input wire [1:0] hilo_weM,
-
 	input wire[4:0] writeregM,
 	input wire regwriteM,
 	input wire memtoregM,
 
 	//write back stage
-
-	input wire [1:0] hilo_weW,
-
 	input wire[4:0] writeregW,
 	input wire regwriteW
     );
 
 	wire lwstallD,branchstallD;
 
-
-
-
-	assign div_start = (alucontrolE == `EXE_DIV_OP&& div_ready == 1'b0)?1'b1:
-				   (alucontrolE == `EXE_DIV_OP && div_ready == 1'b1)?1'b0:
-				   (alucontrolE == `EXE_DIVU_OP && div_ready == 1'b0)?1'b1:
-				   (alucontrolE == `EXE_DIVU_OP && div_ready == 1'b1)?1'b0:
-			       1'b0;
-
-
-
 	//forwarding sources to D stage (branch equality)
 	assign forwardaD = (rsD != 0 & rsD == writeregM & regwriteM);
 	assign forwardbD = (rtD != 0 & rtD == writeregM & regwriteM);
-	assign forwardajalE = (alucontrolE == `EXE_JAL_OP||alucontrolE == `EXE_JALR_OP)?1'b1:1'b0;
+	
 	//forwarding sources to E stage (ALU)
-	assign forwardhiloE =	(hilo_weE==2'b00 & (hilo_weM==2'b10 | hilo_weM==2'b01 | hilo_weM==2'b11))?2'b01:
-							(hilo_weE==2'b00 & (hilo_weW==2'b10 | hilo_weW==2'b01 | hilo_weW==2'b11))?2'b10:
-							2'b00;
-	
-	
-	
-	
+
 	always @(*) begin
 		forwardaE = 2'b00;
 		forwardbE = 2'b00;
@@ -121,11 +85,10 @@ module hazard(
 				(writeregE == rsD | writeregE == rtD) |
 				memtoregM &
 				(writeregM == rsD | writeregM == rtD));
-	assign #1 stallD = div_start | lwstallD | branchstallD;
+	assign #1 stallD = lwstallD | branchstallD;
 	assign #1 stallF = stallD;
-	assign #1 stallE = div_start | branchstallD;
-	assign #1 stallM = branchstallD;
 		//stalling D stalls all previous stages
+	assign #1 flushE = stallD;
 		//stalling D flushes next stage
 	// Note: not necessary to stall D stage on store
   	//       if source comes from load;
